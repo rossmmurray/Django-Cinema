8ea318c5f02efca0fe54baaafb44bc3999bfa96c
Ross Murray <rossmichaelm@gmail.com>
2018-12-04 21:38:49 +0000

    added films index view and url

--- films/models.py
+++ films/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Films(models.Model):
+	title = models.CharField(max_length=200)
+	description = models.TextField()
+	image = models.ImageField(upload_to='images/')
