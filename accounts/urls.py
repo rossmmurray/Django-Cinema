0dabca6f7653be423d0ae5469e9ac847679a38ab
Ross Murray <rossmichaelm@gmail.com>
2018-12-06 23:16:52 +0000

    Done signup page

--- accounts/urls.py
+++ accounts/urls.py
@@ -20,5 +20,5 @@ from . import views
 
 urlpatterns = [
     path('signup', views.signup, name='signup'),
-    path('login', views.signup, name='login'),
+    path('login', views.login, name='login'),
 ]
