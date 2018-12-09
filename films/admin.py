6205f99dd7b5ec05af927c77bcfe8856dc7349e4
Ross Murray <rossmichaelm@gmail.com>
2018-12-09 17:45:24 +0000

    added seats model

--- films/admin.py
+++ films/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from films.models import Film, Screening
+from films.models import Film, Screening, Seat
+
 
 admin.site.register(Film)
 admin.site.register(Screening)
+admin.site.register(Seat)
--- films/models.py
+++ films/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Film(models.Model):
@@ -19,3 +21,22 @@ class Screening(models.Model):
 
 	def __str__(self):
 		return f'{self.film.title} at {self.date_time: %I:%M %p} on {self.date_time: %d/%m/%y }'
+
+
+class Seat(models.Model):
+	seat_no = models.IntegerField()
+	screening = models.ForeignKey(Screening, on_delete=models.CASCADE)
+	available = models.BooleanField()
+
+	def __str__(self):
+		return f'Seat {self.seat_no} for {self.screening.__str__()}'
+
+
+@receiver(post_save, sender=Screening)
+def create_seats(sender, instance, created, *args, **kwargs):
+	"""Create all seats with Availability=True on creation of a screening."""
+	if created:
+		for i in range(36):
+			new_seat = Seat(seat_no=i+1, screening_id=instance.id, available=True)
+			new_seat.save()
+			print(new_seat)
